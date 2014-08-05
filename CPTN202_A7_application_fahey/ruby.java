468d70d8997b7473397a9b307438fbfa93fbca9d
Paul Fahey <pfahey182@gmail.com>
2014-08-05 12:44:26 -0400

    initial commit of project files

---
+++ CPTN202_A7_application_fahey/ruby.java
@@ -0,0 +1,16 @@
+
+public class ruby extends diamond
+{
+	 public ruby() 
+	    { 
+	        initialize();
+	    } 
+	 
+	    private void initialize()
+	    { 
+	        super.set_amount(amount);
+	        super.set_player(player);
+	        super.set_date(found); 
+	        return;
+	    }
+}
